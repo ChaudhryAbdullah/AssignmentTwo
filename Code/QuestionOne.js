1e3f1c0d2ba0362af82ba40bb4720ad297b53011
ChaudhryAbdullah <chaudhryabdullah91@hotmail.com>
2020-07-22 15:11:30 -0700

    Assingmentquestions

---
+++ Code/QuestionOne.js
@@ -0,0 +1,18 @@
+for(var i=0;i<100;i++)
+{
+
+    if(i%2==0&&i%5==0)
+    {
+        console.log('TooFoo');
+    }
+    else if(i%2==0)
+    {
+        console.log('Too');
+    }
+    else if(i%5==0)
+    {
+       console.log('Foo');
+    }
+
+    
+}
---
+++ Code/QuestionTwo.js
@@ -0,0 +1,8 @@
+var x=0;
+for(var i=1;i<=20;i++)
+{
+ x=x+i;
+
+}
+console.log('Result =');
+console.log(x);
